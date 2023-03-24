e511537c494e71af521f28ddc257abaaa4340a5f
Joabesv <joabevarjao123@gmail.com>
2023-03-24 13:33:17 -0300

    upload files and treat other infos

--- src/helpers/createDir.ts
+++ src/helpers/createDir.ts
@@ -2,8 +2,9 @@ import { existsSync, mkdirSync } from 'node:fs';
 import { join, resolve } from 'node:path';
 
 export function ensureDir() {
-  const dir = resolve(join(__dirname, 'upload'));
+  const dir = resolve(join(__dirname, '../../tmp'));
   if (!existsSync(dir)) {
     mkdirSync(dir);
   }
+  return dir;
 }
--- src/modules/upload/upload.routes.ts
+++ src/modules/upload/upload.routes.ts
@@ -1,12 +1,19 @@
 import { FastifyInstance } from 'fastify';
+import { Multipart } from '@fastify/multipart';
 import { createWriteStream } from 'node:fs';
 import { pipeline } from 'node:stream/promises';
+import { ensureDir } from '@/helpers/createDir';
 
 export async function uploadRoutes(app: FastifyInstance) {
   app.post('/', async (request, reply) => {
-    const parts = request.files();
+    const dir = ensureDir();
+    const parts: AsyncIterableIterator<Multipart> = request.parts();
     for await (const part of parts) {
-      await pipeline(part.file, createWriteStream(`./tmp/${part.filename}`));
+      if ('file' in part) {
+        await pipeline(part.file, createWriteStream(`${dir}/${part.filename}`));
+      } else {
+        request.log.info(part.fieldname, 'files');
+      }
     }
     return { message: 'files uploaded' };
   });
